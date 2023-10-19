655a3593d6f9669789473bd9f478222113f71fbb
QuocManh2002 <manhnq2002@gmail.com>
2023-10-19 23:24:46 +0700

    add color tag

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2ECC71);
+const lightPrimaryColor = Color(0xFF82E0AA);
+const primaryTextColor = Color(0xFF000000);
+const lightPrimaryTextColor = Color(0xFFF2F2F2);
+const secondaryColor = Color(0xFF0FA3E2);
+const redColor = Color(0xFFFF3D3D);
+const yellowColor = Color(0xFFFFB23F);
+const purpleColor = Color(0xFFB580FA);
+
+
---
+++ lib/constants/tags.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:greenwheel_user_app/models/tag.dart';
+
+List<Tag> tags = [
+  Tag(id: "1", title: "Hồ", mainColor: Colors.white, strokeColor: Color(0xFF59CDFF)),
+  Tag(id: "2", title: "Núi", mainColor: Colors.white, strokeColor: Color(0xFFE79901)),
+  Tag(id: "3", title: "Biển", mainColor: Colors.white, strokeColor: Color(0xFF006590)),
+  Tag(id: "4", title: "Suối", mainColor: Colors.white, strokeColor: Color(0xFF5869FF)),
+  Tag(id: "5", title: "Rừng", mainColor: Colors.white, strokeColor: Color(0xFF007613)),
+  Tag(id: "6", title: "Hang động", mainColor: Colors.white, strokeColor: Color(0xFF5C645E)),
+  Tag(id: "7", title: "Đồi cát", mainColor: Colors.white, strokeColor: Color(0xFFFFE500)),
+  Tag(id: "8", title: "Thác", mainColor: Colors.white, strokeColor: Color(0xFF31FFE6)),
+  Tag(id: "9", title: "Tắm", mainColor: Color(0xFF59CDFF), strokeColor: Colors.black),
+  Tag(id: "10", title: "Cắm trại", mainColor: Color(0xFF82E0AA), strokeColor: Colors.black),
+  Tag(id: "11", title: "Leo trèo", mainColor: Color(0xFFE79901), strokeColor: Colors.black),
+  Tag(id: "12", title: "Chèo thuyền", mainColor: Color(0xFF01E7BD), strokeColor: Colors.black),
+  Tag(id: "13", title: "Lặn", mainColor: Color(0xFF96A1FF), strokeColor: Colors.black),
+  Tag(id: "14", title: "Lướt sóng", mainColor: Color(0xFFA5FFEF), strokeColor: Colors.black),
+  Tag(id: "15", title: "Câu cá", mainColor: Color(0xFF49B9F9), strokeColor: Colors.black),
+  Tag(id: "16", title: "Xuân", mainColor: Color(0xFF82E0AA), ),
+  Tag(id: "17", title: "Hạ", mainColor: Color(0xFFFFB967), ),
+  Tag(id: "18", title: "Thu", mainColor: Color(0xFFF0D984), ),
+  Tag(id: "19", title: "Đông", mainColor: Color(0xFF33D8D8), ),
+  Tag(id: "20", title: "M.Bắc", mainColor: Color(0xFF8DFF71), strokeColor: Color(0xFF00889A)),
+  Tag(id: "21", title: "M.Trung", mainColor: Color(0xFF6EDCFF), strokeColor: Color(0xFF7B1BC6)),
+  Tag(id: "22", title: "M.Nam", mainColor: Color(0xFFFFD771), strokeColor: Color(0xFFFFD771)),
+  Tag(id: "23", title: "Tỉnh", mainColor: Colors.white, strokeColor: Colors.black),
+];
