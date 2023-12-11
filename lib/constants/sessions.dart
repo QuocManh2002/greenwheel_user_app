6ad2dea451d906a24187f45f29ebea52ba30d8d1
nthingg <thinhnguyenngoc1424@gmail.com>
2023-12-12 03:38:26 +0700

    ThinhNN - Modify toast

--- lib/constants/sessions.dart
+++ lib/constants/sessions.dart
@@ -1,8 +1,9 @@
+import 'package:greenwheel_user_app/constants/urls.dart';
 import 'package:greenwheel_user_app/models/session.dart';
 
 const List<Session> sessions = [
-  Session(name: "Buổi sáng", range: "06:00 SA - 10:00 SA", image: ""),
-  Session(name: "Buổi trưa", range: "10:00 SA - 14:00 CH", image: ""),
-  Session(name: "Buổi chiều", range: "14:00 CH - 18:00 CH", image: ""),
-  Session(name: "Buổi tối", range: "18:00 CH - 23:00 CH", image: ""),
+  Session(name: "Buổi sáng", range: "06:00 SA - 10:00 SA", image: morning),
+  Session(name: "Buổi trưa", range: "10:00 SA - 14:00 CH", image: noon),
+  Session(name: "Buổi chiều", range: "14:00 CH - 18:00 CH", image: afternoon),
+  Session(name: "Buổi tối", range: "18:00 CH - 23:00 CH", image: evening),
 ];
--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,4 +1,8 @@
 const String baseImage = "assets/images/";
+const String morning = "${baseImage}period_01.png";
+const String noon = "${baseImage}period_02.png";
+const String afternoon = "${baseImage}period_03.png";
+const String evening = "${baseImage}period_04.png";
 const String gcoin_logo = "${baseImage}gcoin_logo.svg";
 const String male_icon = "${baseImage}male.png";
 const String female_icon = "${baseImage}female.png";
